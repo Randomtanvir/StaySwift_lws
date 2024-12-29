8e85d14abf3f30ae4863c2a08bc2ca86ec0bfc07
Tanvir <tanvir.info07@gmail.com>
2024-12-29 23:46:46 +0600

    hotel details page ready

--- components/hotel/details/Gallery.jsx
+++ components/hotel/details/Gallery.jsx
@@ -1,14 +1,32 @@
-const Gallery = () => {
+import Image from "next/image";
+
+const Gallery = ({ gallery }) => {
+  const newImageGall = [...gallery];
+  newImageGall.shift();
+  console.log(newImageGall);
+
   return (
     <section className="container">
       <div className="grid grid-cols-2 imageshowCase">
-        <img src="./assets/images/1.png" className="h-[400px]" alt="" />
+        <Image
+          src={gallery[0] || "/1.png"}
+          className="h-[400px]"
+          alt="Main pic"
+          width={600}
+          height={600}
+          quality={100}
+        />
 
         <div className="grid grid-cols-2 grid-rows-2 h-[400px]">
-          <img src="./assets/images/2.png" alt="" />
-          <img src="./assets/images/3.png" alt="" />
-          <img src="./assets/images/4.png" alt="" />
-          <img src="./assets/images/5.png" alt="" />
+          {newImageGall.map((img, i) => (
+            <Image
+              src={img || `/${i + 1}.png`}
+              key={img}
+              alt="sub pic"
+              width={400}
+              height={400}
+            />
+          ))}
         </div>
       </div>
     </section>
--- db/queries/index.js
+++ db/queries/index.js
@@ -1,9 +1,38 @@
 import { hotelModel } from "@/models/hotel-model";
+import { ratingModel } from "@/models/rating-model";
+import { reviewModel } from "@/models/review-model";
 import { dbConnect } from "@/service/momgo";
-import { replaceMongoIdInArray } from "@/utils/data-util";
+import {
+  replaceMongoIdInArray,
+  replaceMongoIdInObject,
+} from "@/utils/data-util";
 
 export const getAllHotels = async () => {
   await dbConnect();
-  const hotels = await hotelModel.find().lean();
+  const hotels = await hotelModel
+    .find()
+    .select([
+      "thumbNailUrl",
+      "name",
+      "highRate",
+      "lowRate",
+      "city",
+      "propertyCategory",
+    ])
+    .lean();
   return replaceMongoIdInArray(hotels);
+};
+
+export const getHotelById = async (hotelId) => {
+  const hotel = await hotelModel.findById(hotelId).lean();
+  return replaceMongoIdInObject(hotel);
+};
+
+export const getRatingsForHotel = async (hotelId) => {
+  const rating = await ratingModel.find({ hotelId }).lean();
+  return replaceMongoIdInArray(rating);
+};
+export const getReviewsForHotel = async (hotelId) => {
+  const rating = await reviewModel.find({ hotelId: hotelId }).lean();
+  return replaceMongoIdInArray(rating);
 };
